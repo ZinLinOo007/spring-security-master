56074896ad18e49a1ef73d82a6cb9a8780582d99
xnine <mgzinlin1000@gmial.com>
2022-10-14 09:45:29 +0630

    config

--- src/main/java/com/example/springsecuritymaster/security/WebSecurityConfig.java
+++ src/main/java/com/example/springsecuritymaster/security/WebSecurityConfig.java
@@ -79,6 +79,8 @@ public class WebSecurityConfig {
                 .mvcMatchers("/bootstrap/**")
                 .permitAll()
                 .mvcMatchers("/customers").hasRole(CUSTOMERS_PAG_VIEW)
+                .mvcMatchers("/employees").hasRole(EMPLOYEES_PAGE_VIEW)
+                .mvcMatchers("/departments").hasRole(DEPARTMENTS_PAGE_VIEW)
                 .anyRequest()
                 .authenticated()
                 .and()
